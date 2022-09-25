7db5d0990b8009c36fb8da6bd5612b3e17ef70bd
rubis-vladimir <84345727+rubis-vladimir@users.noreply.github.com>
2022-09-26 00:01:07 +0300

    Добавлен проект

---
+++ Palette/Palette/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Palette
+//
+//  Created by Владимир Рубис on 25.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
